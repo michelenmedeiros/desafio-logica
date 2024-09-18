2f0a1c15fa53e187b392865db33ac49c4b9489a1
Michelen <michelenms@gmail.com>
2024-09-18 19:20:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,34 +10,34 @@ for (let i = 0; i < herois.length; i++) {
     let xp = xpHerois[i];
     let classificacao = " ";
 
-//Estrutura de descisão
-if(xp < 1000) {
-    classificacao = "Ferro";
-}
-else if (xp >= 1001 && xp <= 2000) {
-    classificacao = "Bronze";
-}
-else if (xp >= 2001 && xp <= 3000) {
-    classificacao = "Prata";
-}
-else if (xp >= 3001 && xp <= 4000) {
-    classificacao = "Ouro";
-}
-else if (xp >= 4001 && xp <= 5000) {
-    classificacao = "Platina";
-}
-else if (xp >= 5001 && xp <= 6000) {
-    classificacao = "Diamante";
-}
-else if (xp >= 6001 && xp <= 8000) {
-    classificacao = "Ascendente";
-}
-else if (xp >= 8001 && xp <= 10000) {
-    classificacao = "Imortal";
-}
-else (xp >= 10001) {
-    classificacao = "Radiante";
-}
+    //Estrutura de descisão
+    if(xp < 1000) {
+        classificacao = "Ferro";
+    }
+    else if (xp >= 1001 && xp <= 2000) {
+        classificacao = "Bronze";
+    }
+    else if (xp >= 2001 && xp <= 3000) {
+        classificacao = "Prata";
+    }
+    else if (xp >= 3001 && xp <= 4000) {
+        classificacao = "Ouro";
+    }
+    else if (xp >= 4001 && xp <= 5000) {
+        classificacao = "Platina";
+    }
+    else if (xp >= 5001 && xp <= 6000) {
+        classificacao = "Diamante";
+    }
+    else if (xp >= 6001 && xp <= 8000) {
+        classificacao = "Ascendente";
+    }
+    else if (xp >= 8001 && xp <= 10000) {
+        classificacao = "Imortal";
+    }
+    else (xp >= 10001) {
+        classificacao = "Radiante";
+    }
 
 //Mensagem fixa
 console.log(heroiMensagem + nomeHeroi + nivelHeroiMensagem + classificacao)
